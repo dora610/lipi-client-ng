ac79a6e14bdca4881a4cbeaa6e2124cd39afc244
Surajit Karuri <surajitkaruri@gmail.com>
2022-12-06 01:00:05 +0530

    basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarLeftComponent } from './sidebar-left/sidebar-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    BlogPageComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarLeftComponent
   ],
   imports: [
     BrowserModule,
